49ddfbcfd4d766ca93a340d70c1b3f46e80be8cf
firesquid6 <jdeiss06@gmail.com>
2025-02-28 15:59:58 -0600

    add a basic server

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+import { Stripe } from "stripe";
+
+interface Kit {
+  stripe: Stripe,
+  secret: string,
+}
+
+const kit: Kit = {
+  stripe: new Stripe(process.env.STRIPE_API_KEY!),
+  secret: process.env.STRIPE_WEBHOOK_SECRET!,
+}
+
+const app = new Elysia()
+  .state("kit", kit)
+  .get("/", () => {
+    console.log("hello, world!");
+  })
+  .post("/webhook", (ctx) => {
+    console.log(ctx.body);
+  })
+
+
+app.listen(3000, () => {
+  console.log("App started in port 3000");
+});
